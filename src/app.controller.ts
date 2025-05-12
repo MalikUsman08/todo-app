4b9bd53fecb7c19c63bf850aa06b9c9052c3cfa6
MalikUsman08 <musman0841@gmail.com>
2025-05-12 23:02:44 +0500

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private dataSource: DataSource,
-    private configService: ConfigService,
+    private readonly dataSource: DataSource,
+    private readonly configService: ConfigService,
   ) {}
 
   @Get()
