0740da0309fecbd3824009c7bbef1e6c6bc30e3a
MalikUsman08 <musman0841@gmail.com>
2025-05-13 01:09:22 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,34 +12,18 @@ import { AppService } from './app.service';
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        const dbUrl = configService.get<string>('DATABASE_URL');
-        console.log('Database URL:', dbUrl ? 'URL is set' : 'URL is not set');
-        console.log('NODE_ENV:', configService.get<string>('NODE_ENV'));
-
-        return {
-          type: 'postgres' as const,
-          url: dbUrl,
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          migrations: [__dirname + '/migrations/*{.ts,.js}'],
-          migrationsRun: true, // This will run migrations automatically
-          synchronize: false, // Disable synchronize in production
-          ssl:
-            configService.get<string>('NODE_ENV') === 'production'
-              ? { rejectUnauthorized: false }
-              : false,
-          logging: true,
-          connectTimeoutMS: 10000, // 10 seconds
-          extra: {
-            connectionTimeoutMillis: 10000,
-            query_timeout: 10000,
-            statement_timeout: 10000,
-          },
-          retryAttempts: 10,
-          retryDelay: 3000, // 3 seconds
-          keepConnectionAlive: true,
-        };
-      },
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        migrationsRun: true,
+        synchronize: false,
+        ssl:
+          configService.get('NODE_ENV') === 'production'
+            ? { rejectUnauthorized: false }
+            : false,
+      }),
       inject: [ConfigService],
     }),
     TasksModule,
