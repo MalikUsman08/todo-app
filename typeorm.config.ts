4067f539317799e169b551a119664c097cb1f9cb
MalikUsman08 <musman0841@gmail.com>
2025-05-12 23:36:46 +0500

    Create typeorm.config.ts

---
+++ typeorm.config.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  url: configService.get('DATABASE_URL'),
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*{.ts,.js}'],
+  ssl:
+    configService.get('NODE_ENV') === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
+});
